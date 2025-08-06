0f0391eb847faa3dc1a03cf4695930211ba0f2e3
Jonathan <148803412+jflorez-giraldo@users.noreply.github.com>
2025-08-06 10:32:17 -0500

    Update app_pca_nhanes_filtros.py

--- app_pca_nhanes_filtros.py
+++ app_pca_nhanes_filtros.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import prince
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
